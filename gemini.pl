651197c567e5cf4b9559011817d9fec2593e568c
MagnificentPako <my@procrastination.life>
2020-05-13 17:07:00 +0200

    added all status codes

--- gemini.pl
+++ gemini.pl
@@ -8,10 +8,25 @@ status(success(Num, Meta)) :-
     write_status(2, Num, Meta).
 status(success(Meta)) :- status(success(0, Meta)).
 
+status(redirect(Num, Meta)) :-
+    proper_secondary_status(Num),
+    write_status(3, Num, Meta).
+status(redirect(Meta)) :- status(redirect(0, Meta)).
+
+status(temporary_failure(Num, Meta)) :-
+    proper_secondary_status(Num),
+    write_status(4, Num, Meta).
+status(temporary_failure(Meta)) :- status(temporary_failure(0, Meta)).
+
 status(permanent_failure(Num, Meta)) :-
     proper_secondary_status(Num),
     write_status(5, Num, Meta).
 status(permanent_failure(Meta)) :- status(permanent_failure(0, Meta)).
+
+status(cert_required(Num, Meta)) :-
+    proper_secondary_status(Num),
+    write_status(6, Num, Meta).
+status(cert_required(Meta)) :- status(cert_required(0, Meta)).
 
 status(Status) :- 
     format(string(S), "Internal server error wrong status: ~w", Status),
